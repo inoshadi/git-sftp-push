0ea3d98d4854fd5dc74ad77291cda7d68cf3968f
Sutrisno Hadi <inoshadi@gmail.com>
2019-08-04 03:11:28 +0700

    manage delete to the remote

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 if [[ $# -eq 0 ]] ; then
-    echo 'No argument supplied'
+    echo 'No argument supplied for deployment'
     echo 'usage: deploy.sh [usr@domain.xxx:/path/to/target/]'
     exit 1
 fi
 echo "staging..."
 CURRDIR=`dirname $0`
-$CURRDIR/sftpPush.sh
-echo "push staged.."
-sftp $1 < /tmp/sftpPush.txt
+FN_GD="/tmp/sftpPush_tmp.$RANDOM"
+echo '' > $FN_GD
+FN_TMP="/tmp/sftpPush.$RANDOM"
+echo '' > $FN_TMP
+$CURRDIR/sftpPush.sh "$FN_GD" "$FN_TMP"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+sed -r '/^\s*$/d' "$FN_TMP" > $FN_GD
+LIN=`wc -l $FN_GD | cut -f 1 -d " "`
+echo "Provide your sftp password to executing $LIN line(s) of command to: $1"
+echo "or (Ctrl + c) to abort"
+sftp $1 < "$FN_GD"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "Removing files: $FN_GD and $FN_TMP .."
+rm -f $FN_GD
+rm -f $FN_TMP
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
--- sftpPush.sh
+++ sftpPush.sh
@@ -1,25 +1,68 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo 'Required 2 arguments supplied for generating sftp command'
+    echo 'usage: sftpPush.sh [tmp_filename_1] [tmp_filename_2]'
+    exit 1
+fi
 pwd
-FN_GD=/tmp/sftpPush_tmp.txt
+
+FN_GD=$1
 echo '' > $FN_GD
-FN_TMP=/tmp/sftpPush.txt
+FN_TMP=$2
 echo '' > $FN_TMP
-git diff --name-only `git rev-parse @~` `git rev-parse @` > $FN_GD
+FN_TMP2="${FN_TMP}2"
+
+if [ `git rev-list --count HEAD` == 1 ]; then
+    echo "Currently you've got only one commit available"
+    git log --oneline --name-only `git rev-parse @` > $FN_TMP  
+    sed '1d' $FN_TMP > $FN_GD
+    echo '' > $FN_TMP
+    
+else
+    git diff --name-only `git rev-parse @~` `git rev-parse @` > $FN_TMP
+    cat $FN_TMP > $FN_GD
+    echo '' > $FN_TMP
+fi
+
 echo "Staged file(s):"
 cat $FN_GD
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+LIN=`wc -l $FN_GD | cut -f 1 -d " "`
+
+echo "Processing ${LIN} line(s) of $FN_GD....."
 while IFS= read -r FN
 do
   if test -f "${FN}"; then
-    echo "put ${FN} ${FN}" >> $FN_TMP
-  else 
-    echo "rm ${FN}" >> $FN_TMP
-  fi
-done < "$FN_GD"
+    git log --name-only --oneline --follow "$FN" >$FN_TMP2
+    sed -i '1~2d' $FN_TMP2
+    FNB=`sed '2!d' $FN_TMP2`
+    
+    FNA=`sed '1!d' $FN_TMP2`
+    if [ "$FNB" != "" ]; then
+        if [ "$FNB" != "$FNA" ] ; then
 
-#printf '%s\n' "${FN}"
-#sed -i -e 's/\(.*\)/\1 \1/' $FN_TMP
-#sed -i -e 's/^/put /' $FN_TMP
-#echo "quit" >> $FN_TMP
+            if test -f "${FNB}"; then
+                echo "";
+            else
+                echo "File has been renamed from $FNB to $FNA"
+                echo "rm ${FNB}" >> $FN_TMP
+                printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+            fi
+            echo "put ${FNA} ${FNA}" >> $FN_TMP
+            
+        else 
+            echo "put ${FN} ${FN}" >> $FN_TMP
+        fi
+    else 
+        echo "put ${FN} ${FN}" >> $FN_TMP
+    fi
+  else
+    echo "File has been deleted: $FN"
+    echo "rm ${FN}" >> $FN_TMP
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+  fi
+  
+done < "$FN_GD"
+echo "SFTP Commands generated: "
 cat $FN_TMP
 
